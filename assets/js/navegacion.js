a2d33894c2f857d8fdf2b5b614360bfd86762449
jotaefe95 <81982789+juanfco95@users.noreply.github.com>
2023-04-27 16:10:09 +0200

    Actualizacion Local Storage

--- assets/js/navegacion.js
+++ assets/js/navegacion.js
@@ -13,4 +13,49 @@ function cargarContenido(nombreArchivo) {
         $("header").removeClass("header-transparente");
     }
     //$('#main').load(nombreArchivo);
-}  
+}
+
+function selectCafeteria(nombreArchivo) {
+    cargarContenido(nombreArchivo);
+    storeCafeteria();
+}
+
+function storeCafeteria() {
+    if (typeof (Storage) != 'undefined') {
+        console.log(Storage);
+        /*Con esto en caso de tener ya 5 cafeterías visitadas lo que hariamos
+        es quedarnos con las 4 ultimas para despues añadir la visitada ahora*/
+        let historialPrevio = JSON.parse(localStorage.getItem("Cafeterias Visitadas"));
+        let historialNuevo = [];
+        if (historialPrevio != null) {
+            if (historialPrevio.includes("Cafeteria Parabellum")) {
+                let posicion = historialPrevio.indexOf("Cafeteria Parabellum");
+                console.log(posicion)
+                console.log(historialPrevio.length)
+                for (let i = posicion; i <= historialPrevio.length - 2; i++) {
+                    historialPrevio[i] = historialPrevio[i + 1];
+                    console.log(i)
+                }
+                historialNuevo = historialPrevio;
+                historialNuevo[historialPrevio.length - 1] = "Cafeteria Parabellum"
+                console.log(historialPrevio);
+            } else {
+                if (historialPrevio.length >= 5) {
+                    for (let i = 1; i < historialPrevio.length; i++) {
+                        historialNuevo[i - 1] = historialPrevio[i];
+                    }
+                }
+            }
+        }
+        /*Tener en cuenta que ahora añadimos manualmente el nombre de la cafeteria
+      pero la idea seria que ese nombre se obtenga del mismo JSON por ejemplo*/
+        historialNuevo = historialNuevo.concat("Cafeteria Parabellum")
+        localStorage.setItem("Cafeterias Visitadas", JSON.stringify(historialNuevo));
+        //Aqui simplemente lo imprimo por consola, pero el getItem nos servirá para obtener la
+        //información del historial de visitas
+        console.log(localStorage.getItem("Cafeterias Visitadas"))
+    } else {
+        alert("Storage no es compatible con este navegador");
+    }
+}
+
