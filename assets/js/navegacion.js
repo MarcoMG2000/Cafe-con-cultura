1bd7ae33ee8307803a3e1b872670900f90da3c5e
JordiSM <61203146+JordiSM@users.noreply.github.com>
2023-05-11 09:24:23 +0800

    Implemented Map Section with Dinamic Markers
    integrado section de Map con la página principal, cafeterias a partir del JSON y demás

--- assets/js/navegacion.js
+++ assets/js/navegacion.js
@@ -29,6 +29,40 @@ function cargarContenido(nombreArchivo, nombreCafeteria, nombreEvento) {
     });
 }
 
+function cargarContenidoMap(){
+  cargarContenido("map.html");
+
+  scriptHtml = document.getElementById("script-api")
+  if(scriptHtml){
+    scriptHtml.remove();
+  }
+
+  var script = document.createElement('script');
+
+  /**
+   * 
+   * Author:
+   * https://github.com/JordiSM
+   * 
+   * Replace API_KEY and MAP_ID with your own ones
+   * 
+   * Option1: Map with Food and Monuments Markers
+   * <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBaIKV0ipX-n7b_RwquptRgH70iszT3G5Y&map_ids=ab4de70ad7fea2e0&callback=initMap"></script>
+   * 
+   * Option2: Map with NO Markers
+   * <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBaIKV0ipX-n7b_RwquptRgH70iszT3G5Y&map_ids=fcbac43d02c5a043&callback=initMap"></script>
+   * 
+   * NOTE: This API_Key is restricted. More about this here:
+   * https://developers.google.com/maps/api-security-best-practices?hl=es-419
+   * 
+  **/
+  script.src = "https://maps.googleapis.com/maps/api/js?key=AIzaSyBaIKV0ipX-n7b_RwquptRgH70iszT3G5Y&map_ids=fcbac43d02c5a043&callback=initMap";
+  script.async = true;
+  window.initMap = initMap;
+
+  document.head.appendChild(script);
+}
+
 function selectCafeteria(nombreArchivo) {
   cargarContenido(nombreArchivo);
   storeCafeteria();
