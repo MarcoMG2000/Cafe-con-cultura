68f6a132818f4ebb564484599fff0a777be83f05
Marco Martinez <marcomg2000@gmail.com>
2023-05-15 17:04:20 +0200

    Corrección de HTML y CSS

--- assets/js/navegacion.js
+++ assets/js/navegacion.js
@@ -916,7 +916,7 @@ async function cargarBusquedaEvent(listaCafeterias, filtros, primeraVez) {
   }
   var cafeteriaSelect = document.getElementById("busqueda-filtro");
   var pagina = '<div class="row-filt-ev">';
-  pagina += '<div id="past-events" class="col-lg-6 aos-init aos-animate" data-aos="fade-left" data-aos-delay="100">';
+  pagina += '<div id="past-events" class="col-12 aos-init aos-animate" data-aos="fade-left" data-aos-delay="100">';
 
   let resultados = 0;
 
